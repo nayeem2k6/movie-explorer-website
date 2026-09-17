9ac4c4e3a37abd0db2238d14cece38244e09af42
Nayeem Babu <nayeembabu2k6@gmail.com>
2026-09-18 00:03:06 +0800

    MovieModal

--- src/components/movieModal.jsx
+++ src/components/movieModal.jsx
@@ -72,7 +72,7 @@ const MovieModal = ({ movie, onClose }) => {
             className="close-button"
             onClick={onClose}
           >
-            close 
+            close
           </button>
         </div>
       </div>
